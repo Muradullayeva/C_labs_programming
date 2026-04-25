f5d79eabb13b8b3c8b7e607e2d3bba767f1b35ca
Fionia_m <sevinchmuradullayeva29@gmail.com>
2026-04-25 15:09:29 +1000

    Add files via upload

--- lab7/main.c
+++ lab7/main.c
@@ -1,32 +1,49 @@
 #include <stdio.h>
-#include <string.h>
+
+#define COUNT 20
 
 typedef struct {
     char name[20];
+    char patronymic[20];
     char lastname[20];
     int year;
-
-}humen;
+} humen;
 
 int main() {
-    humen a[4], b[4];
+    humen a[COUNT], b[COUNT];
     int i, j;
     humen temp;
 
-    for (i = 0; i < 4; i++) {
-        printf("Человек %d:\n ", i + 1);
-        printf("Имя: "); scanf("%s", a[i].name);
-        printf("Фамилия: "); scanf("%s", a[i].lastname);
-        printf("Год: "); scanf("%d", &a[i].year);
+    FILE *file = fopen("humen.txt", "r");
+    if (file == NULL) {
+        printf("Ошибка: файл humen.txt не найден!\n");
+        return 1;
     }
 
-    for (i = 0; i < 4; i++) {
+    // Читаем 4 слова: Имя + Отчество + Фамилия + Год
+    for (i = 0; i < COUNT; i++) {
+        fscanf(file, "%s", a[i].name);
+        fscanf(file, "%s", a[i].patronymic);
+        fscanf(file, "%s", a[i].lastname);
+        fscanf(file, "%d", &a[i].year);
+    }
+    fclose(file);
+
+    printf("Загружено %d человек:\n\n", COUNT);
+    for (i = 0; i < COUNT; i++) {
+        printf("%d. %s %s %s, %d\n", i+1,
+               a[i].name, a[i].patronymic, a[i].lastname, a[i].year);
+    }
+
+    // Копирование
+    for (i = 0; i < COUNT; i++) {
         b[i] = a[i];
     }
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (b[j].year > b[i].year) {
+    // Сортировка
+    for (i = 0; i < COUNT - 1; i++) {
+        for (j = 0; j < COUNT - 1 - i; j++) {
+            if (b[j].year > b[j+1].year) {
                 temp = b[j];
                 b[j] = b[j+1];
                 b[j+1] = temp;
@@ -34,9 +51,11 @@ int main() {
         }
     }
 
-    printf ("По возрастанию года рождения:\n");
-    for (i = 0; i < 4; i++) {
-        printf("%s %s, %d\n", b[i].name, b[i].lastname, b[i].year);
+    printf("\nПо возрастанию года рождения:\n");
+    for (i = 0; i < COUNT; i++) {
+        printf("%s %s %s, %d\n",
+               b[i].name, b[i].patronymic, b[i].lastname, b[i].year);
     }
+
     return 0;
 }
